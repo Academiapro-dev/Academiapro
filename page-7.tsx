bd2be1b0d76d370091eb4c1d7f84c9823bb212f0
Academiapro-dev <jacklalou26@gmail.com>
2026-06-04 13:17:32 +0200

    Update page-7.tsx

--- page-7.tsx
+++ page-7.tsx
@@ -433,7 +433,7 @@ export default function HomePage() {
         </section>
 
       </main>
-      <Footer />
+      
 
       <style>{`
         @keyframes float { 0%,100%{transform:translateY(0)} 50%{transform:translateY(-15px)} }
