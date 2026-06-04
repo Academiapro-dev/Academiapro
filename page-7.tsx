6428c37750570aa3b121584d0146234808138fee
Academiapro-dev <jacklalou26@gmail.com>
2026-06-04 13:37:04 +0200

    Update page-7.tsx

--- page-7.tsx
+++ page-7.tsx
@@ -1,6 +1,6 @@
 
 import Link from 'next/link'
-import { FORMATIONS, AGENTS, PRATICIENS_BIENETRE } from '@/lib/data'
+import { FORMATIONS, AGENTS, PRATICIENS_BIENETRE } from './data'
 
 export default function HomePage() {
   const topFormations = FORMATIONS.slice(0, 8)
