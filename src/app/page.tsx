c5caa80ee7593b7755cbf3feed3d1356165cd977
Academiapro-dev <jacklalou26@gmail.com>
2026-06-04 18:42:11 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Link from 'next/link'
-import { FORMATIONS, AGENTS, PRATICIENS_BIENETRE } from './data'
+import { FORMATIONS, AGENTS, PRATICIENS_BIENETRE } from '../../data'
 
 export default function HomePage() {
   const topFormations = FORMATIONS.slice(0, 8)
