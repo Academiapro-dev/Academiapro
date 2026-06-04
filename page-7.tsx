61e163381dad70d427ded85af09e81642d63ce97
Academiapro-dev <jacklalou26@gmail.com>
2026-06-04 11:36:04 +0200

    Update page-7.tsx

--- page-7.tsx
+++ page-7.tsx
@@ -1,5 +1,4 @@
-import Nav from '@/components/layout/Nav'
-import Footer from '@/components/layout/Footer'
+
 import Link from 'next/link'
 import { FORMATIONS, AGENTS, PRATICIENS_BIENETRE } from '@/lib/data'
 
