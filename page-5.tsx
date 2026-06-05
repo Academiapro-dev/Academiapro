b8d3bacc6ecb8d3442c17037135bd585ebe38103
Academiapro-dev <jacklalou26@gmail.com>
2026-06-05 20:13:53 +0200

    Update page-5.tsx

--- page-5.tsx
+++ page-5.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState } from 'react'
 import Link from 'next/link'
-import { FORMATIONS } from './data'
+import { FORMATIONS } from './src/data'
 
 
 export default function FinancementPage() {
