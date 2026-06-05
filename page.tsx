1c8d7f12e9d854908c7e6849c31957a0c78ec73a
Academiapro-dev <jacklalou26@gmail.com>
2026-06-05 02:48:58 +0200

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Link from 'next/link'
 import { FORMATIONS, AGENTS, PRATICIENS_BIENETRE } from '../../data'
 
+
 export default function HomePage() {
   const topFormations = FORMATIONS.slice(0, 8)
   const topAgents = AGENTS.slice(0, 6)
