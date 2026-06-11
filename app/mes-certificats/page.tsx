3e971c6e30574ed119230df4da8953293207b358
Academiapro-dev <jacklalou26@gmail.com>
2026-06-11 15:47:08 +0200

    Fix: app/mes-certificats/page.tsx

--- app/mes-certificats/page.tsx
+++ app/mes-certificats/page.tsx
@@ -464,3 +464,4 @@ export default async function MesCertificatsPage() {
                                   fontSize: "11px",
                                   fontWeight: "600",
                                   letterSpacing
+}}}}}}}
