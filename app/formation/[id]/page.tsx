5ddca5c6a3c6c16c16b28b1d2bc7a7d3387734e1
Academiapro-dev <jacklalou26@gmail.com>
2026-08-10 21:58:30 +0200

    Update page.tsx

--- app/formation/[id]/page.tsx
+++ app/formation/[id]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"; 
 import { useTraductionAuto } from "../../../hooks/useTraductionAuto";
 
 // LES ACCENTS SONT OBLIGATOIRES ICI.
